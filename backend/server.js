e8e8f15f1d2ec64600192ef1b2e182e676c295a3
Muhammad Arslan <imarslan7061@gmail.com>
2025-02-18 01:39:18 +0500

    update
    Project Completed

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import path from "path"
 
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
@@ -12,11 +13,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve()
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
-app.use(helmet()); // Helmet is a security middlware that helps you Protect Your App by setting various HTTP Headers
+app.use(helmet({
+  contentSecurityPolicy:false
+})); // Helmet is a security middlware that helps you Protect Your App by setting various HTTP Headers
 app.use(morgan("dev"));
 
 // Apply Arcjet rate-limit to all routes
@@ -52,6 +56,15 @@ app.use(async (req, res, next) => {
 
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // server our react app
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 async function initDB() {
   try {
